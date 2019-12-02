826f65b3e2eab678bec759a1d450d30cc385afe5
nincas <nincas21@gmail.com>
2019-12-02 15:53:22 +0800

    updates

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Style from './Theme/style';
 import Router from './Router/Router'
+import { Helmet} from "react-helmet/es/Helmet";
+
 
 ReactDOM.render(
     <React.Fragment>
+        <Helmet>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
+        </Helmet>
         <Style/>
         <Router />
     </React.Fragment>, 
