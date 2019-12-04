1da6e5c67c37a374a8726664a61fd2c1d647a976
nino.invech3e <nino.invech3e@gmail.com>
2019-12-05 05:20:20 +0800

    updates

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Style from './Theme/style';
 import Router from './Router/Router';
+import { Helmet } from 'react-helmet';
 import ReallySmoothScroll from 'really-smooth-scroll';
 
 ReallySmoothScroll.shim();
 ReactDOM.render(
     <React.Fragment>
+        <Helmet>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
+        </Helmet>
         <Style/>
         <Router />
     </React.Fragment>, 
