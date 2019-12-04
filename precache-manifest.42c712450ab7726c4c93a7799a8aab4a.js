cf2ea241fb4303bf33776172da003dc4e7349288
nincas <nincas21@gmail.com>
2019-12-04 13:28:15 +0800

    Updates

---
+++ precache-manifest.42c712450ab7726c4c93a7799a8aab4a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ebbce29d734631a3c27",
+    "url": "/static/js/6.7ebbce29.chunk.js"
+  },
+  {
+    "revision": "9835b3f86420bd12d79c",
+    "url": "/static/js/0.9835b3f8.chunk.js"
+  },
+  {
+    "revision": "612b6feebeb59167b5d9",
+    "url": "/static/js/main.612b6fee.chunk.js"
+  },
+  {
+    "revision": "506f96421445c3d6db93",
+    "url": "/static/js/runtime~main.506f9642.js"
+  },
+  {
+    "revision": "715a41cedfe625a41af8fe741aa3b478",
+    "url": "/static/media/NIÑO-MANALO-CASUPANAN.715a41ce.docx"
+  },
+  {
+    "revision": "cdcda47a85a6fc2028a0",
+    "url": "/static/js/3.cdcda47a.chunk.js"
+  },
+  {
+    "revision": "778fb5a76742a1a74d37",
+    "url": "/static/js/4.778fb5a7.chunk.js"
+  },
+  {
+    "revision": "8d762e89885631d03ce1",
+    "url": "/static/js/5.8d762e89.chunk.js"
+  },
+  {
+    "revision": "e071d186cd72c29451d8cd7af6d4bb51",
+    "url": "/static/media/dart-project.e071d186.MP4"
+  },
+  {
+    "revision": "c5d015f58fec0769ec42",
+    "url": "/static/js/7.c5d015f5.chunk.js"
+  },
+  {
+    "revision": "8b0766f4a31b44441a05",
+    "url": "/static/js/8.8b0766f4.chunk.js"
+  },
+  {
+    "revision": "5a29045450807d5de297",
+    "url": "/static/js/9.5a290454.chunk.js"
+  },
+  {
+    "revision": "d078968bc905921e539a6f954e1e2ca1",
+    "url": "/static/media/bg-image.d078968b.jpg"
+  },
+  {
+    "revision": "63586af6c1d71b6e85ca1cacf35cfcae",
+    "url": "/static/media/php.63586af6.png"
+  },
+  {
+    "revision": "612b6feebeb59167b5d9",
+    "url": "/static/css/main.94667ca2.chunk.css"
+  },
+  {
+    "revision": "cdcda47a85a6fc2028a0",
+    "url": "/static/css/3.1cc07d90.chunk.css"
+  },
+  {
+    "revision": "dc2180bd57094648d85ccc8bd3830417",
+    "url": "/index.html"
+  }
+];
