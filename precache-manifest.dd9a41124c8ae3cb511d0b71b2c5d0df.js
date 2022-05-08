a0a2248a15799db5ba5f354e984256ce8b77d5be
nnocsupnn <nmcasupanan@medicardphils.com>
2022-05-09 01:17:40 +0800

    Updates

---
+++ precache-manifest.dd9a41124c8ae3cb511d0b71b2c5d0df.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba2ed1eb93d24aafe870",
+    "url": "/static/css/main.5989023f.chunk.css"
+  },
+  {
+    "revision": "ba2ed1eb93d24aafe870",
+    "url": "/static/js/main.ba2ed1eb.chunk.js"
+  },
+  {
+    "revision": "b02d3f04a80fecd8780a",
+    "url": "/static/js/runtime~main.b02d3f04.js"
+  },
+  {
+    "revision": "d9a78355f03a5938c064",
+    "url": "/static/css/2.2cf86031.chunk.css"
+  },
+  {
+    "revision": "d9a78355f03a5938c064",
+    "url": "/static/js/2.d9a78355.chunk.js"
+  },
+  {
+    "revision": "fd244939ea68948ebbb6",
+    "url": "/static/js/3.fd244939.chunk.js"
+  },
+  {
+    "revision": "aa211a31fd2c1afab84b",
+    "url": "/static/js/4.aa211a31.chunk.js"
+  },
+  {
+    "revision": "013bcebb338e61e6f24d",
+    "url": "/static/js/5.013bcebb.chunk.js"
+  },
+  {
+    "revision": "0b8bcc7d15a3177f165c",
+    "url": "/static/js/6.0b8bcc7d.chunk.js"
+  },
+  {
+    "revision": "db10c80cebc070076dd1",
+    "url": "/static/js/7.db10c80c.chunk.js"
+  },
+  {
+    "revision": "c15df4cfe96a5508161f",
+    "url": "/static/js/8.c15df4cf.chunk.js"
+  },
+  {
+    "revision": "d078968bc905921e539a6f954e1e2ca1",
+    "url": "/static/media/bg-image.d078968b.jpg"
+  },
+  {
+    "revision": "63586af6c1d71b6e85ca1cacf35cfcae",
+    "url": "/static/media/php.63586af6.png"
+  },
+  {
+    "revision": "e071d186cd72c29451d8cd7af6d4bb51",
+    "url": "/static/media/dart-project.e071d186.MP4"
+  },
+  {
+    "revision": "efeffb75fd099effd8963052c4426a34",
+    "url": "/static/media/NINO-MANALO-CASUPANAN-CV-20220420.efeffb75.docx"
+  },
+  {
+    "revision": "ed291358199198d8ae11c113f11ef13b",
+    "url": "/index.html"
+  }
+];
