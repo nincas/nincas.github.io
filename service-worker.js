4f4833c490cc85eea3bbe122ade5a4afa778c569
nnocsupnn <nmcasupanan@medicardphils.com>
2022-05-10 21:32:44 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1718ab665d1a226004632f3fdd8c4c3c.js"
+  "/precache-manifest.d80f9ae95b513fa2865aec2a4a52f1f9.js"
 );
 
 workbox.clientsClaim();
